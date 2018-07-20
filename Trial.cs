c41b27b92f3bdf251bf275ce84fb46e28cbd6003
Adexandria <40776751+Adexandria@users.noreply.github.com>
2018-07-21 00:43:31 +0100

    Create Trial.cs

---
+++ Trial.cs
@@ -0,0 +1,21 @@
+ public Trial {
+public static void main (String[] args) {
+string Firstname = "Adeola";
+string Lastname = "Aderibgbe";
+DateTime Birtdate;
+ Birthdate = new
+DateTime(1999,12,11)
+string Addresssline = "Rumuodara";
+string City="Port-harcourt"
+string State="Rivers";
+int Zipcode= 500192
+string Nation="Nigeria"
+int Age=18;
+
+Console.WriteLine("{0} [1]", Lastname,Firstname);
+Console.WriteLine("[0] years old",Age);
+Console.WriteLine(Street+ ","+City +"," +Nation);
+Console.WriteLine($"Born on{Birthdate}");
+                }
+}
+     
